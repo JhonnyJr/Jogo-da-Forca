422dda70acf33e3534b3e9f63d28f1678598a4ea
JhonnyJr <38056461+JhonnyJr@users.noreply.github.com>
2018-05-25 09:04:47 -0300

    Update forca.js

--- forca.js
+++ forca.js
@@ -1,5 +1,5 @@
 //Criando um "vetor" para as palavras. Para adicionar palavras é só colocar a palavra desejada: ( "palavra", )
-var words = ["carro", "queijo", "jogo", "tigela", "batata", "janela", "parede"];
+var palavras = ["carro", "queijo", "jogo", "tigela", "batata", "janela", "parede"];
 
 //Esoclhendo uma palavra aleatória, dentre as opções da variável "palavras"
 var palavra = palavras[Math.floor(Math.random() * palavras.length)];
